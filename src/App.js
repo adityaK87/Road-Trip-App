4dad5a589180d9cc256d370a5a9e8d595065b391
Aditya kumar <adityapal8763244@gmail.com>
2022-09-29 19:21:59 +0530

    getting map & deleting unuseful files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
+import tt from '@tomtom-international/web-sdk-maps';
 
-function App() {
+const App = () => {
+  const mapElement = useRef();
+  // eslint-disable-next-line
+  const [map, setMap] = useState({});
+  const [longitude, setLongitude] = useState(-0.112869);
+  const [latitude, setLatitude] = useState(51.504)
+
+  // for india
+  // const longitude = 78.8718;
+  // const latitude = 21.7679;
+
+  useEffect(() => {
+    // console.log("aditya")
+    const map = tt.map({
+      key: process.env.REACT_APP_TOM_TOM_KEY,
+      container: mapElement.current,
+      center: [longitude, latitude],
+      stylesVisibility: {
+        trafficIncidents: true,
+        trafficFlow: true
+      },
+      zoom: 14
+    });
+    setMap(map);
+    // eslint-disable-next-line
+  }, [longitude, latitude])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div ref={mapElement} className="map"></div>
+      <div className="searchBar">
+        <h1>
+          Where to?
+        </h1>
+        <input type="text"
+          id='longitude'
+          className='longitude'
+          placeholder='put in longitude'
+          onChange={(e) => { setLongitude(e.target.value) }}
+        />
+        <input type="text"
+          id='latitude'
+          className='latitude'
+          placeholder='put in latitude'
+          onChange={(e) => { setLatitude(e.target.value) }}
+        />
+      </div>
     </div>
+
+
   );
 }
 
